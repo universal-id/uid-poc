79473f8680b5245e778e45a29a89c85f0e30bdcf
Ali Bayat <ali.bayat.gh@gmail.com>
2022-01-14 11:56:08 +0330

    Enable swagger in production mode

--- source/online-poc/contacts-service/Program.cs
+++ source/online-poc/contacts-service/Program.cs
@@ -24,11 +24,8 @@ builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
